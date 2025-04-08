88b2ae15f195bc0f56c9f9050e7904fc1a5ce7ee
praveen909 <praveenalajangi369@gmail.com>
2025-04-08 22:59:59 +0530

    made the 3 dice random

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const mybutton = document.getElementById("mybutton");
-const mylabel = document.getElementById("mylabel");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
 
-let min = 50;
-let max = 100;
+let min = 1;
+let max = 6;
 
-let randomNum;
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
 mybutton.onclick = function () {
-  randomNum = Math.floor(Math.random() * (max - min)) + min;
-  mylabel.textContent = randomNum;
+  randomNum1 = Math.floor(Math.random() * (max - min)) + min;
+  label1.textContent = randomNum1;
+  randomNum2 = Math.floor(Math.random() * (max - min)) + min;
+  label2.textContent = randomNum2;
+  randomNum3 = Math.floor(Math.random() * (max - min)) + min;
+  label3.textContent = randomNum2;
 };
